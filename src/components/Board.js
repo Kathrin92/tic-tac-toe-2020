a588d887e3be6c794a0d2a50da8899639b3c945a
Kathrin92 <kathrinulmer@gmx.de>
2020-03-10 12:45:14 +0100

    Lifting state up

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,25 +2,33 @@ import React from "react";
 import Square from "./Square";
 
 export default function Board() {
+  const [squares, setSquares] = React.useState(Array(9).fill(null));
+
   const status = "Next player: X";
+
+  function handleClick(squareIndex) {
+    const squaresCopy = squares.slice();
+    squaresCopy[squareIndex] = "X";
+    setSquares(squaresCopy);
+  }
 
   return (
     <div>
       <div className="status">{status}</div>
       <div className="board-row">
-        <Square value="0" />
-        <Square value="1" />
-        <Square value="2" />
+        <Square value={squares[0]} onClick={() => handleClick(0)} />
+        <Square value={squares[1]} onClick={() => handleClick(1)} />
+        <Square value={squares[2]} onClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square value="3" />
-        <Square value="4" />
-        <Square value="5" />
+        <Square value={squares[3]} onClick={() => handleClick(3)} />
+        <Square value={squares[4]} onClick={() => handleClick(4)} />
+        <Square value={squares[5]} onClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square value="6" />
-        <Square value="7" />
-        <Square value="8" />
+        <Square value={squares[6]} onClick={() => handleClick(6)} />
+        <Square value={squares[7]} onClick={() => handleClick(7)} />
+        <Square value={squares[8]} onClick={() => handleClick(8)} />
       </div>
     </div>
   );
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 export default function Square(props) {
-  const [value, setValue] = React.useState(null);
+  // const [value, setValue] = React.useState(null);
   // arey destructure. Konstante hate zwei werte. setValue= funktion welche meinen wert (value) ändert
   return (
-    <button className="square" onClick={() => setValue("🦠")}>
-      {value}
+    <button className="square" onClick={props.onChlick}>
+      {props.value}
     </button>
     // '=>' nicht vergessen
   );
